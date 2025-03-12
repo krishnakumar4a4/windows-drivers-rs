6f52f1fd02997f70fc9ebffa0e8a90ae77cb391c
Gurinder Singh <gurisingh@microsoft.com>
2025-03-12 09:55:20 +0530

    Replaced closures with functions in sample driver

--- examples/sample-kmdf-safe-driver/src/lib.rs
+++ examples/sample-kmdf-safe-driver/src/lib.rs
@@ -3,9 +3,9 @@
 #![no_std]
 
 use wdf::{
-    driver_entry, object_context, println, trace, CancellableMarkedRequest, Device,
-    DeviceInit, Driver, Guid, IoQueue, IoQueueConfig, NtError, NtStatus, SpinLock,
-    Timer, TimerConfig
+    driver_entry, object_context, println, trace, CancellableMarkedRequest, Request,
+    RequestCancellationToken, Device, DeviceInit, Driver, Guid, IoQueue,
+    IoQueueConfig, NtError, NtStatus, SpinLock, Timer, TimerConfig
 };
 
 #[object_context(IoQueue)]
@@ -36,71 +36,11 @@ fn device_add(device_init: &mut DeviceInit) -> Result<(), NtError> {
     let mut queue_config = IoQueueConfig::default();
 
 
-    queue_config.evt_io_read = Some(|queue, request, _| {
-        println!("Safe Rust evt_io_read called");
-
-        if let Some(context) = QueueContext::get(&queue) {
-            println!("Request processing started");
-
-            match request.mark_cancellable(|token| {
-                println!("Request evt_cancel called");
-
-                let queue = token.get_io_queue();
-
-                if let Some(context) = QueueContext::get(&queue) {
-                    let mut req = context.request.lock();
-                    if let Some(req) = req.take() {
-                        match req.unmark_cancellable() {
-                            Ok(req) => {
-                                req.complete(NtStatus::cancelled());
-                                println!("Request cancelled");
-                            }
-                            Err(e) => {
-                                println!("Failed to unmark request as cancellable: {e:?}");
-                            }
-                        }
-                    }
-                } else {
-                    println!("Could not cancel request. Failed to get queue context");
-                }
-            }) {
-                Ok(cancellable_req) => {
-                    *context.request.lock() = Some(cancellable_req);
-                    let _ = context.timer.start(5000);
-
-                    println!("Request marked as cancellable");
-                }
-                Err(e) => {
-                    println!("Failed to mark request as cancellable: {e:?}");
-                }
-            }
-        } else {
-            println!("Failed to get queue context");
-        }
-    });
+    queue_config.evt_io_read = Some(evt_io_write);
 
     let mut queue = IoQueue::create(&device, &queue_config)?;
 
-    let timer_config = TimerConfig::new_non_periodic(&queue, |timer| {
-        println!("Safe Rust evt_timer_func called");
-        if let Some(queue) = timer.get_parent_object::<IoQueue>() {
-            let context = QueueContext::get(&queue).unwrap();
-            let mut req = context.request.lock();
-            if let Some(req) = req.take() {
-                match req.unmark_cancellable() {
-                    Ok(req) => {
-                        req.complete(NtStatus::cancelled());
-                        println!("Request cancelled");
-                    }
-                    Err(e) => {
-                        println!("Failed to unmark request as cancellable: {e:?}");
-                    }
-                };
-            }
-        }
-
-        timer.stop(false);
-    });
+    let timer_config = TimerConfig::new_non_periodic(&queue, evt_timer);
 
     let timer = Timer::create(&timer_config)?;
 
@@ -113,4 +53,70 @@ fn device_add(device_init: &mut DeviceInit) -> Result<(), NtError> {
 
     trace("Trace: Safe Rust device add complete");
     Ok(())
+}
+
+fn evt_io_write(queue: &mut IoQueue, request: Request, _length: usize) {
+    println!("Safe Rust evt_io_read called");
+
+    if let Some(context) = QueueContext::get(&queue) {
+        println!("Request processing started");
+
+        match request.mark_cancellable(evt_request_cancel) {
+            Ok(cancellable_req) => {
+                *context.request.lock() = Some(cancellable_req);
+                let _ = context.timer.start(5000);
+
+                println!("Request marked as cancellable");
+            }
+            Err(e) => {
+                println!("Failed to mark request as cancellable: {e:?}");
+            }
+        }
+    } else {
+        println!("Failed to get queue context");
+    }
+}
+
+fn evt_request_cancel(token: RequestCancellationToken) {
+    println!("Request evt_cancel called");
+
+    let queue = token.get_io_queue();
+
+    if let Some(context) = QueueContext::get(&queue) {
+        let mut req = context.request.lock();
+        if let Some(req) = req.take() {
+            match req.unmark_cancellable() {
+                Ok(req) => {
+                    req.complete(NtStatus::cancelled());
+                    println!("Request cancelled");
+                }
+                Err(e) => {
+                    println!("Failed to unmark request as cancellable: {e:?}");
+                }
+            }
+        }
+    } else {
+        println!("Could not cancel request. Failed to get queue context");
+    }
+}
+
+fn evt_timer(timer: &mut Timer) {
+    println!("Safe Rust evt_timer_func called");
+    if let Some(queue) = timer.get_parent_object::<IoQueue>() {
+        let context = QueueContext::get(&queue).unwrap();
+        let mut req = context.request.lock();
+        if let Some(req) = req.take() {
+            match req.unmark_cancellable() {
+                Ok(req) => {
+                    req.complete(NtStatus::cancelled());
+                    println!("Request cancelled");
+                }
+                Err(e) => {
+                    println!("Failed to unmark request as cancellable: {e:?}");
+                }
+            };
+        }
+    }
+
+    timer.stop(false);
 }
