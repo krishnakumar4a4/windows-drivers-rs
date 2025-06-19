ff067e527aab71feacb4fe580a441bf5b708a8e1
Gurinder Singh <gurisingh@microsoft.com>
2025-06-19 18:54:32 +0530

    A small change in logging in driver unload

--- crates/wdf/src/api/driver.rs
+++ crates/wdf/src/api/driver.rs
@@ -141,11 +141,11 @@ extern "C" fn evt_driver_device_add(
 }
 
 extern "C" fn driver_unload(driver: *mut DRIVER_OBJECT) {
-    println!("Driver unload called");
+    println!("Driver unload");
 
     clean_up_tracing(driver);
 
-    println!("Driver unloaded");
+    println!("Driver unload done");
 }
 
 /// Calls the safe driver entry function
