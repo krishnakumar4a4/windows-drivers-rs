589d034746a8ba0b3da7ebda624393d1afa5bc2b
Gurinder Singh <frederick.the.fool@gmail.com>
2025-09-13 15:25:03 +0530

    Implemented evt_io_write and completion routine

--- examples/usb-fx2/src/bulkrwr.rs
+++ examples/usb-fx2/src/bulkrwr.rs
@@ -1,5 +1,4 @@
 use wdf::{
-    Device,
     IoQueue,
     IoTarget,
     Request,
@@ -86,17 +85,74 @@ fn evt_request_read_completion_routine(request: Request, _target: &IoTarget) {
     request.complete_with_information(status.into(), bytes_read);
 }
 
-pub fn evt_io_write(_queue: &IoQueue, request: Request, length: usize) {
+pub fn evt_io_write(queue: &IoQueue, mut request: Request, length: usize) {
     println!("I/O write callback called");
 
     if length > TEST_BOARD_TRANSFER_BUFFER_SIZE {
         println!(
-            "Requested write length {} exceeds maximum of {}",
+            "Transfer {} exceeds {}",
             length, TEST_BOARD_TRANSFER_BUFFER_SIZE
         );
         request.complete_with_information(status_codes::STATUS_INVALID_PARAMETER.into(), 0);
         return;
     }
+
+    let device_context =
+        DeviceContext::get(queue.get_device()).expect("Device context should be set");
+    let pipe = device_context.get_bulk_write_pipe();
+
+    if let Err(e) =
+        pipe.format_request_for_write(&mut request, RequestFormatBuffer::RequestBuffer(None))
+    {
+        println!("Format request for write failed: {:?}", e);
+        request.complete_with_information(e.code().into(), 0);
+        return;
+    }
+
+    request.set_completion_routine(evt_request_write_completion_routine);
+
+    let io_target = pipe.get_io_target();
+
+    if let Err(request) = request.send_asynchronously(&io_target) {
+        let status = request.get_status();
+        println!("Request send failed: {:?}", status);
+        request.complete_with_information(status, 0);
+    }
+}
+
+fn evt_request_write_completion_routine(request: Request, _target: &IoTarget) {
+    let completion_params = request.get_completion_params();
+    let status = completion_params.io_status.status;
+
+    let RequestCompletionParamDetails::Usb {
+        completion: usb_completion_params,
+        ..
+    } = completion_params.parameters
+    else {
+        println!("Request completed with Non-USB completion params");
+        request.complete_with_information(status_codes::STATUS_INVALID_DEVICE_REQUEST.into(), 0);
+        return;
+    };
+
+    let UsbRequestCompletionParamDetails::PipeWrite {
+        length: bytes_written, ..
+    } = usb_completion_params.parameters
+    else {
+        println!("Request completed with Non-USB pipe write completion params");
+        request.complete_with_information(status_codes::STATUS_INVALID_DEVICE_REQUEST.into(), 0);
+        return;
+    };
+
+    if status.is_success() {
+        println!("Number of bytes written: {}", bytes_written);
+    } else {
+        println!(
+            "Write failed - request status {:?} UsbdStatus {:?}",
+            status, usb_completion_params.usbd_status
+        );
+    }
+
+    request.complete_with_information(status.into(), bytes_written);
 }
 
 pub fn evt_io_stop(
