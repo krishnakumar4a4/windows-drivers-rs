e029da85aef056db96caa078d34f043e4a9da9b6
Gurinder Singh <frederick.the.fool@gmail.com>
2025-09-22 17:35:42 +0530

    Fixed the echo sample build

--- examples/sample-kmdf-safe-driver/src/lib.rs
+++ examples/sample-kmdf-safe-driver/src/lib.rs
@@ -135,11 +135,11 @@ fn device_create(device_init: &mut DeviceInit) -> NtResult<()> {
         Some(evt_device_self_managed_io_suspend);
     pnp_power_callbacks.evt_device_self_managed_io_restart = Some(evt_device_self_managed_io_start);
 
-    let device = Device::create(device_init, Some(pnp_power_callbacks), None)?;
+    let device = Device::create(device_init, Some(pnp_power_callbacks))?;
 
     // Create a device interface so that applications can find us and talk
     // to us.
-    let _ = device.create_interface(
+    let _ = device.create_device_interface(
         &Guid::parse("2aa02ab1-c26e-431b-8efe-85ee8de102e4").expect("GUID is valid"),
         None,
     )?;
