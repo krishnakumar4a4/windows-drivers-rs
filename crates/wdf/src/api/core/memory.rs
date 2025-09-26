b6a9f978f23e436e78ec7f72b09410f9e21b7287
Gurinder Singh <gurisingh@microsoft.com>
2025-09-26 17:28:40 +0530

    Renamed a method

--- crates/wdf/src/api/core/memory.rs
+++ crates/wdf/src/api/core/memory.rs
@@ -32,17 +32,17 @@ impl_handle! {
 
 impl Memory {
     pub fn get_buffer(&self) -> &[u8] {
-        let (ptr, size) = self.get_buffer_raw_impl();
+        let (ptr, size) = self.get_buffer_raw();
         unsafe { core::slice::from_raw_parts(ptr, size) }
     }
 
     pub fn get_buffer_mut(&mut self) -> &mut [u8] {
-        let (ptr, size) = self.get_buffer_raw_impl();
+        let (ptr, size) = self.get_buffer_raw();
         unsafe { core::slice::from_raw_parts_mut(ptr, size) }
     }
 
     pub fn buffer_len(&self) -> usize {
-        let (_, size) = self.get_buffer_raw_impl();
+        let (_, size) = self.get_buffer_raw();
         size
     }
 
@@ -72,7 +72,7 @@ impl Memory {
         .ok()
     }
 
-    fn get_buffer_raw_impl(&self) -> (*mut u8, usize) {
+    fn get_buffer_raw(&self) -> (*mut u8, usize) {
         let mut buf_size = 0;
         let buffer = unsafe {
             call_unsafe_wdf_function_binding!(
